d186edcd770428c701d88910b3deff48cd6a6a18
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-07-19 00:04:05 +0530

    UI Changes (Made todo box green)

--- src/pages/Todos.jsx
+++ src/pages/Todos.jsx
@@ -35,31 +35,56 @@ const Todos = () => {
     fetchTodos();
   }, []);
 
+  const toggleTodoStatus = async (_id, currentStatus) => {
+    try {
+      const response = await axios.put(
+        `http://127.0.0.1:8000/update-status/${_id}`,
+        { status: !currentStatus },
+        { headers: { "Content-Type": "application/json" } }
+      );
+
+      setTodos((prevTodos) =>
+        prevTodos.map((todo) =>
+          todo._id === _id ? { ...todo, status: !currentStatus } : todo
+        )
+      );
+    } catch (err) {
+      console.error("Error updating status:", err);
+      alert("Failed to update todo status.");
+    }
+  };
+
+  const deleteTodo = async (_id, currentStatus) => {
+    try {
+      const response = await axios.delete(
+        `http://127.0.0.1:8000/delete-todo/${_id}`
+      );
+      setTodos((prevTodos) => prevTodos.filter((todo) => todo._id !== _id));
+    } catch (err) {
+      console.error("Error updating status:", err);
+      alert("Failed to update todo status.");
+    }
+  };
+
   return (
     <div className="todos-container">
       <div className="todos-box">
-        {todos.map((todo, index) => (
-          // <div key={index} className="todos-todo">
-          //   <div>
-          //     <strong>{todo.title}</strong>
-          //   </div>
-          //   <div>{todo.description}</div>
-          //   <div>Status: {todo.status ? "✅ Done" : "🕓 Pending"}</div>
-          // </div>
+        {todos.map((todo) => (
           <Accordion
+            key={todo._id}
             sx={{
               mb: 2,
               minHeight: "10vh",
               display: "flex",
               justifyContent: "center",
               flexDirection: "column",
-              color: todo.status ? "green" : "black"
+              backgroundColor: todo.status ? "rgba(104, 188, 125, 0.36)" : "white",
             }}
           >
             <AccordionSummary
               expandIcon={<ExpandMoreIcon />}
               aria-controls="panel3-content"
-              id="panel3-header"
+              id={`panel3-header-${todo._id}`}
             >
               <Typography component="span">
                 <strong>{todo.title}</strong>
@@ -67,7 +92,10 @@ const Todos = () => {
             </AccordionSummary>
             <AccordionDetails>{todo.description}</AccordionDetails>
             <AccordionActions>
-              <Button>{todo.status ? "remove" : "complete"}</Button>
+              <Button onClick={() => toggleTodoStatus(todo._id, todo.status)}>
+                {todo.status ? "Undo" : "Complete"}
+              </Button>
+              <Button onClick={() => deleteTodo(todo._id)}>Remove</Button>
             </AccordionActions>
           </Accordion>
         ))}
