143aa47214f118e0abaa48eabd326fe34d047684
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-07-25 22:32:02 +0530

    ask permission for notifications

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,16 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
 import SignUp from "./pages/SignUp";
 import InApp from "./pages/InApp";
+import { useEffect } from "react";
 
 function App() {
+  useEffect(() => {
+    if ("Notification" in window && Notification.permission !== "granted") {
+      Notification.requestPermission().then((permission) => {
+        console.log("Notification permission:", permission);
+      });
+    }
+  }, []);
   return (
     <Router>
       <Routes>
