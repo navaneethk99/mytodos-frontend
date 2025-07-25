1ad033ec1c5b83bfdf073b71cd75164dd4405cb7
LordSlushii <88372623+LordSlushii@users.noreply.github.com>
2025-07-25 23:52:10 +0530

    sa

--- src/pages/InApp.jsx
+++ src/pages/InApp.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { Routes, Route } from "react-router-dom";
 import BottomNav from "../components/BottomNav";
 import "../stylesheets/inapp.css";
